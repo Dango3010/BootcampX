203c8de18896d1dce0aee7091d1c70f0cd146a8c
Dango3010 <thuyoanh199930@gmail.com>
2023-01-12 03:44:55 +0000

    adding 4_queries folder and its 13 task files, completing the last assignment of week 11

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -3,6 +3,14 @@
 -- psql
 -- \c bootcampx;
 
+-- * all tables in bootcampx database:
+-- 1. assignment_submissions table: id, assignment_id, student_id, submission_date, duration
+-- 2. assignments table: id, name, content, duration, day, chapter
+-- 3. cohorts table: id, name, start_date, end_date
+-- 4. students table: id, name, email, phone, github, start_date, end_date, cohort_id
+-- 5. teachers: id, name, is_active, start_date, end_date
+-- 6. assistance_requests: id, student_id, teacher_id, assignment_id, created_at, started_at, completed_at, student_feedback, teacher_feedback
+
 CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY NOT NULL, 
   name VARCHAR(255) NOT NULL,
